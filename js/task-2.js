94bcfb0a97a6d46859ff5c1340ea23e88a9da200
VanUaTu1 <vkapustjak@gmail.com>
2023-12-03 14:53:30 +0200

    HW 2 done

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,16 @@
 "use strict";
+function formatMessage(message, maxLength) {
+    let newMessage;
+    if (maxLength < message.length) {
+        newMessage = message.slice(0, maxLength) + "...";
+    }
+    else newMessage = message;
+    return newMessage;
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
